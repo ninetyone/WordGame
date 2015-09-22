d59f7989b35d356284d92ad0d00b0c6a3260fc93
Akshay <akshay.g174@gmail.com>
2015-09-22 20:01:09 +0530

    Before splash screen

--- app/src/main/java/com/example/akshaygoyal/wordgame/MainActivity.java
+++ app/src/main/java/com/example/akshaygoyal/wordgame/MainActivity.java
@@ -7,11 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements MainActivityFragment.OnGameClearedListener {
+public class MainActivity extends AppCompatActivity implements MainActivityFragment.OnGameClearedListener  {
 
-    public static final String MY_PREFS = "MyPrefs";
+    public static final String MY_PREFS = "MyPrefs" ;
     SharedPreferences sharedPreferences;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +18,28 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
         if (!sharedPreferences.contains("count")) {
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putInt("count", 1);
-            editor.apply();
+            editor.commit();
         }
         if (!sharedPreferences.contains("last_updated_row")) {
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putInt("last_updated_row", 1);
             editor.commit();
         }
+        if (!sharedPreferences.contains("last_high_score")) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("last_high_score", 20);
+            editor.commit();
+        }
+        if (!sharedPreferences.contains("score_flag")) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("score_flag", false);
+            editor.commit();
+        }
+        //if (!sharedPreferences.contains("current_score")) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("current_score", 20);
+        editor.commit();
+        //}
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, new MainActivityFragment(), "Game Fragment")
@@ -33,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
         setContentView(R.layout.activity_main);
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
